3410893dcc59df738123c7239bc92cd018fbae16
housir <housir@2cdf69c7-79f9-4451-ab89-d57ae126d6d6>
2013-10-24 20:07:54 +0000

    git-svn-id: http://jrt.choury.com/svn/myos@29 2cdf69c7-79f9-4451-ab89-d57ae126d6d6

--- kernel/execve.c
+++ kernel/execve.c
@@ -1,5 +1,6 @@
 #include <syscall.h>
 #include <chouryos.h>
+#include <elf.h>
 
 /*
  execve
@@ -8,6 +9,16 @@
 int sys_execve(char *name, char **argv, char **env) {
     //TODO .....
   // fuck,今天要给周威当苦力了吗
-    errno = ENOMEM;
-    return -1;
+  int fd = sys_open( name );
+  if( fd == -1 ) // fail to open file
+  {
+    exit(0);
+  }
+  else
+  {
+    struct Elf32_Ehdr elf32;
+    sys_close(fd);
+  }
+  errno = ENOMEM;
+  return -1;
 }
